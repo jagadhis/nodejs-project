9a1864faeb1a2041bd0b53cd7f9d22492ecf0d29
jagadhis <jwaran78@gmail.com>
2022-11-13 13:57:59 +0530

    Changes

--- index.js
+++ index.js
@@ -1,82 +1,87 @@
 const express = require('express');
-const Joi = require('joi');
+const Joi = require('joi'); //used for validation
 const app = express();
 app.use(express.json());
-
-const cabs =[
-    {number:'9163',id:1},
-    {number:'3019',id:2},
-    {number:'5836',id:3},
-    {number:'2394',id:4}
-]
-
-// Read Request Handlers
-app.get('/',(req,res)=>{
-    res.send('Hello this is A Nodejs Project For Practicing Nodejs methods');
-})
-
-app.get('/api/cabs',(req,res)=>{
-    res.send(cabs)
-})
-
-app.get('/api/cabs/:id',(req,res)=>{
-    const cab = cabs.find(c => c.id === parseInt(req.params.id));
-    if(!cab)res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>')
-    res.send(cab);
-})
-
-
-// CREATE Request handlers
-app.post('/api/cabs', (req, res)=> {
  
-    const { error } = validatecab(req.body);
-    if (error){
-    res.status(400).send(error.details[0].message)
-    return;
-    }
-    const cab = {
-    id: cabs.length + 1,
-    title: req.body.title
-    };
-    cabs.push(cab);
-    res.send(cab);
-    });
-     
-    //UPDATE Request Handler
-    app.put('/api/cabs/:id', (req, res) => {
-    const cab = cabs.find(c=> c.id === parseInt(req.params.id));
-    if (!cab) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Not Found!! </h2>');
-     
-    const { error } = validatecab(req.body);
-    if (error){
-    res.status(400).send(error.details[0].message);
-    return;
-    }
-     
-    cab.title = req.body.title;
-    res.send(cab);
-    });
-     
-    //DELETE Request Handler
-    app.delete('/api/cabs/:id', (req, res) => {
-     
-    const cab = cabs.find( c=> c.id === parseInt(req.params.id));
-    if(!cab) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
-     
-    const index = cabs.indexOf(cab);
-    cabs.splice(index,1);
-     
-    res.send(cab);
-    });
-     
-    function validatecab(cabs) {
-    const schema = {
-    title: Joi.string().min(3).required()
-    };
-    return Joi.validatecab(cabs, schema);
-     
-    }
+const books = [
+{title: 'Harry Potter', id: 1},
+{title: 'Twilight', id: 2},
+{title: 'Lorien Legacies', id: 3}
+]
+ 
+const schema = Joi.object({
+    title:Joi.string().min(3).required(),
+})
 
+//READ Request Handlers
+app.get('/', (req, res) => {
+res.send('Welcome to Edurekas REST API with Node.js Tutorial!!');
+});
+ 
+app.get('/api/books', (req,res)=> {
+res.send(books);
+});
+ 
+app.get('/api/books/:id', (req, res) => {
+const book = books.find(c => c.id === parseInt(req.params.id));
+ 
+if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
+res.send(book);
+});
+ 
+//CREATE Request Handler
+app.post('/api/books', (req, res)=> {
+ 
+    const validation = schema.validate(req.body);
+if (validation.error){
+res.status(400).send(validation.error.details[0].message)
+return;
+}
+const book = {
+id: books.length + 1,
+title: req.body.title
+};
+books.push(book);
+res.send(book);
+});
+ 
+//UPDATE Request Handler
+app.put('/api/books/:id', (req, res) => {
+const book = books.find(c=> c.id === parseInt(req.params.id));
+if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Not Found!! </h2>');
+ 
+const validation = schema.validate(req.body);
+if (validation.error){
+res.status(400).send(validation.error.details[0].message);
+return;
+}
+ 
+book.title = req.body.title;
+res.send(book);
+});
+ 
+//DELETE Request Handler
+app.delete('/api/books/:id', (req, res) => {
+ 
+const book = books.find( c=> c.id === parseInt(req.params.id));
+if(!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
+ 
+const index = books.indexOf(book);
+books.splice(index,1);
+ 
+res.send(book);
+});
+ 
+// function validateBook(book) {
+// const schema = {
+// title: Joi.string().min(3).required()
+// };
+// return Joi.validate(book, schema);
+ 
+// }
+
+
+ 
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
